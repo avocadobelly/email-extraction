4cee1581c99661630c09a8d96302c51a82d77391
gconnellbass <georgia.connell-bass@altran.com>
2020-09-15 14:20:28 +0100

    added functionality for searching the file for regex pattern

--- extractor.py
+++ extractor.py
@@ -1,1 +1,19 @@
 #Extracts email addresses from a text file
+#pt-1 prints number of email addresses with softwire.com domain
+
+#open the file
+#read the contents line by line
+#check for the substring '@softwire.com'
+
+import re
+with open('sample.txt') as f:
+    count = 0
+    contents = f.read()
+    for i in range(len(contents)):
+        if (contents[i: i + 13] == '@softwire.com'):
+            count = count + 1
+    print(count)
+
+    #pt-2 using regex to find all email addresses:
+    x = re.findall('@\S+', contents)
+    print(len(x))
